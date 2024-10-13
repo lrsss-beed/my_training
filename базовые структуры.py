c5de585170108f14727af4c9054e3b27eaf36041
lrsss-beed <larisanikiforova5959@gmail.com>
2024-10-13 13:51:50 +0800

    Add files via upload

---
+++ базовые структуры.py
@@ -0,0 +1,12 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo','Steve','Khendrik','Aaron'}
+students = list(students)
+students.sort()
+a1 = sum(grades[0])/len(grades[0])
+a2 = sum(grades[1])/len(grades[1])
+a3 = sum(grades[2])/len(grades[2])
+a4 = sum(grades[3])/len(grades[3])
+a5 = sum(grades[4])/len(grades[4])
+average = (a1,a2,a3,a4,a5)
+dict_ = dict(zip(students,average))
+print(dict_)
